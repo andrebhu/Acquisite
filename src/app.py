6ab0f0633adc3b324b03af51077b2e37c20cc7d0
andrebhu@gmail.com <andrebhu@gmail.com>
2022-02-25 19:38:32 -0500

    Setup databases

--- src/app.py
+++ src/app.py
@@ -1,10 +1,54 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
+from models import db, Owner, Post, Searcher
+
+import os
+import warnings
+warnings.filterwarnings('ignore')
 
 
-from models import Student
+# DELETE LATER, CLEARS DATABASE IF FOUND
+if os.path.exists('site.db'):
+    os.remove('site.db')
+    
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = b'\xaa\xc1,g\xcc;\xe6D\xfa-\xf4|\xbd\xe3\xda\x07'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
+app.config['DEBUG'] = True
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+    test = Owner(username='test', email='test@mail.com', password='test')
+    admin = Owner(username='admin', email='admin@mail.com', password='admin')
+
+    db.session.add(test)
+    db.session.add(admin)
+    db.session.commit()
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/')
+def login():
+    pass
+
+@app.route('/register')
+def register():
+
+    pass
+
+@app.route('/home')
+
+
+
+
+# db.session.commit()
+
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
--- src/models.py
+++ src/models.py
@@ -1,11 +1,46 @@
-from app import db
+import hashlib
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
-class Student(db.Model):
+db = SQLAlchemy()
+
+def hash_password(s):
+    if isinstance(s, str):
+        s = s.encode('utf-8')
+    return hashlib.sha1(s).hexdigest()
+
+def verify_password(plaintext, ciphertext):
+    return hash_password(plaintext) == ciphertext
+
+# Business Owner Account
+class Owner(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    balance = db.Column(db.Integer, default=0)
+    posts = db.relationship('Post', backref='owner')
 
     def __repr__(self):
-        return f"Student: {id} {username} {email} {password}"
+        return f'Owner: {self.id} {self.username} {self.email} {self.password}'
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text())
+    owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'))
+
+    def __repr__(self):
+        return f'Post: {self.title} owner_id: {self.owner_id}'
+
+
+# Investor Searcher Account
+class Searcher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    
+    def __repr__(self):
+        return f"Searcher: {self.id} {self.username} {self.email} {self.password}"
