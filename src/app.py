95e7ef794232d4bb7564afbc4577fef0a3a73021
andrebhu@gmail.com <andrebhu@gmail.com>
2022-02-25 20:37:20 -0500

    Added login/register functionality

--- src/app.py
+++ src/app.py
@@ -1,50 +1,82 @@
-from flask import Flask, render_template
-from models import db, Owner, Post, Searcher
+from flask import Flask, render_template, redirect, request, session, url_for, flash
+from models import db, User, Post, verify_password
 
 import os
 import warnings
 warnings.filterwarnings('ignore')
-
-
-# DELETE LATER, CLEARS DATABASE IF FOUND
-if os.path.exists('site.db'):
-    os.remove('site.db')
-    
+ 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = b'\xaa\xc1,g\xcc;\xe6D\xfa-\xf4|\xbd\xe3\xda\x07'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['DEBUG'] = True
 
+
+# DELETE LATER, CLEARS DATABASE IF FOUND
+if os.path.exists('site.db'):
+    os.remove('site.db')
+
 db.init_app(app)
 with app.app_context():
     db.create_all()
 
-    test = Owner(username='test', email='test@mail.com', password='test')
-    admin = Owner(username='admin', email='admin@mail.com', password='admin')
 
-    db.session.add(test)
-    db.session.add(admin)
-    db.session.commit()
-
-
+# ROUTES
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username'].strip()
+        password = request.form['password'].strip()
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            check_password = verify_password(user.password, password)
+            if check_password:
+                session['id'] = user.id
+                session['username'] = user.username
+                return redirect('/home')
+
+        flash('An error occured')
+
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form['username'].strip()
+        email = request.form['email'].strip()
+        password = request.form['password'].strip()
+        account_type = request.form['account_type'].strip()
+
+        try:
+            user = User(username=username, email=email, password=password, account_type=account_type)
+            db.session.add(user)
+            db.session.commit()
+
+            session['id'] = user.id
+            session['username'] = user.username
+            return redirect('/home')
+
+        except:
+            flash('An error occured')   
+    
     return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
 
 @app.route('/home')
 def home():
     return render_template('home.html')
-
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
--- src/models.py
+++ src/models.py
@@ -4,24 +4,24 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-def hash_password(s):
-    if isinstance(s, str):
-        s = s.encode('utf-8')
-    return hashlib.sha1(s).hexdigest()
-
 def verify_password(plaintext, ciphertext):
-    return hash_password(plaintext) == ciphertext
+    return plaintext == ciphertext
 
-# Business Owner Account
-class Owner(db.Model):
+
+'''
+Business Owner Account
+business_owner, investor
+'''
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='owner')
+    account_type = db.Column(db.String(60), nullable=False)
 
     def __repr__(self):
-        return f'Owner: {self.id} {self.username} {self.email} {self.password}'
+        return f'User: {self.id} {self.account_type} {self.username} {self.email} {self.password}'
 
 
 class Post(db.Model):
@@ -29,18 +29,7 @@ class Post(db.Model):
     title = db.Column(db.String(128), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text())
-    owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f'Post: {self.title} owner_id: {self.owner_id}'
-
-
-# Investor Searcher Account
-class Searcher(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-    
-    def __repr__(self):
-        return f"Searcher: {self.id} {self.username} {self.email} {self.password}"
