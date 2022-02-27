63c80dedc4aa85b15151e24f370ad2709a7b0c8c
andrebhu@gmail.com <andrebhu@gmail.com>
2022-02-27 04:01:47 -0500

    Cleanup, fix edit_business

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, request, session, url_for, flash
-from models import db, User, Business, verify_password
+from models import db, User, Business, verify_password, add_data
 
 import os
 import glob
@@ -25,78 +25,9 @@ if os.path.exists('site.db'):
 db.init_app(app)
 with app.app_context():
     db.create_all()
-
-    # Test Data
-    investor = User(
-        first_name='Mark',
-        last_name='Cuban',
-        email='investor@test.com',
-        password='investor',
-        account_type='investor',
-        avatar='pfp.jpg'
-    )
-
-    owner = User(
-        first_name='Andre',
-        last_name='Hu',
-        email='andre@test.com',
-        password='password',
-        account_type='owner'
-    )
-    owner2 = User(
-        first_name='Nick',
-        last_name='Wu',
-        email='nick@test.com',
-        password='password',
-        account_type='owner'
-    )
-    owner3 = User(
-        first_name='Arjun',
-        last_name='Kubal',
-        email='arjun@test.com',
-        password='password',
-        account_type='owner'
-    )
-    db.session.add(investor)
-    db.session.add(owner)
-    db.session.add(owner2)
-    db.session.add(owner3)
-    db.session.commit()
-
+    add_data(db)
+        
     
-    business = Business(
-        name='McDonalds',
-        description=
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-        ,
-        industry='food',
-        size=25,
-        owner=User.query.get(3),
-        image='mcdonalds.png'
-    )
-    
-    business2 = Business(
-        name='Wendys',
-        description='WENDYSWENDYSWENDYSWENDYSWENDYSWENDYSWENDYSWENDYSWENDYSWENDYS',
-        industry='food',
-        size=25,
-        owner=User.query.get(2),
-        image='wendys.png',
-        url='https://www.wendys.com'
-    )
-    business3 = Business(
-        name='Taco Bell',
-        description='We sell tacos',
-        industry='food',
-        size=25,
-        owner=User.query.get(4),
-        image='five_guys.png'
-    )
-    db.session.add(business)
-    db.session.add(business2)
-    db.session.add(business3)
-    db.session.commit()
-
 
 
 # Redirect lost souls
@@ -243,6 +174,7 @@ def create():
             description = request.form['description'].strip()
             industry = request.form['industry'].strip()
             employees = request.form['employees'].strip()
+            url = request.form['url'].strip()
 
             # File uploading
             if 'file' not in request.files:
@@ -260,6 +192,7 @@ def create():
                 description=description,
                 industry=industry,
                 size=employees,
+                url=url,
                 owner=user,
                 image=filename
             )
@@ -339,15 +272,15 @@ def edit(business_id):
                 business.size = int(request.form['employees'].strip())
                 business.url = request.form['url'].strip()
 
-                # # File uploading
-                # if 'file' in request.files:
-                #     file = request.files['file']
-                #     filename = ''
-                #     if file:
-                #         filename = file.filename
-                #         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                # File uploading
+                if 'file' in request.files:
+                    file = request.files['file']
+                    filename = ''
+                    if file:
+                        filename = file.filename
+                        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-                #         business.image = filename
+                        business.image = filename
 
                 
                 db.session.commit()
